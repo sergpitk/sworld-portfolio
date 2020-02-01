64eb5df5556001c81a9318de80f96baabb00755a
sergpitk <sergpitk@andi.lv>
2020-02-01 18:06:52 +0200

    sword 0.022 getProfilerById getProfilerList method

--- src/Controller/ProfilerController.php
+++ src/Controller/ProfilerController.php
@@ -9,9 +9,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProfilerController extends AbstractController
 {
     /**
+     * @Route("/{profiler}", name="profiler-id", methods={"GET", "HEAD"})
+     * @param $profiler
+     * @return RedirectResponse
+     */
+    public function getProfilerById($profiler)
+    {
+        return $this->getProfiler($profiler);
+    }
+
+    /**
      * @Route("/", name="profiler", methods={"GET", "HEAD"})
      */
-    public function index()
+    public function getProfilerList()
     {
         return $this->getProfiler();
     }
@@ -19,8 +29,9 @@ class ProfilerController extends AbstractController
 
     /**
      * @return RedirectResponse
+     * @param $slug
      */
-    protected function getProfiler() {
-        return $this->redirect('_profiler', 301);
+    protected function getProfiler($slug = '') {
+        return $this->redirect("_profiler/".$slug, 301);
     }
 }
