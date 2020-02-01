f84a5e0d9dbf87e6c706f407d3049393bbce4f8b
sergpitk <sergpitk@andi.lv>
2020-02-01 19:30:45 +0200

    sword 0.025 fixtures and faker for Documents

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,44 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Document;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Cocur\Slugify\SlugifyInterface;
 
 class AppFixtures extends Fixture
 {
+    private $faker;
+
+    private $slug;
+
+    public function __construct(SlugifyInterface $slugify)
+    {
+        $this->faker = Factory::create();
+        $this->slug = $slugify;
+    }
+
+    public function loadDocuments(ObjectManager $manager)
+    {
+        for ($i = 1; $i < 20; $i++) {
+            $document = new Document();
+            $document->setTitle($this->faker->text(20));
+            $document->setFile($this->faker->text(20));
+            $document->setCreated((new \DateTime('now')));
+            $document->setLink($this->faker->slug($document->getTitle()));
+            $document->setThumbnail($this->faker->text(20));
+            $document->setUserId($this->faker->randomNumber(2));
+            $manager->persist($document);
+        }
+        $manager->flush();
+    }
+
     public function load(ObjectManager $manager)
     {
         // $product = new Product();
         // $manager->persist($product);
 
-        $manager->flush();
+        $this->loadDocuments($manager);
     }
 }
