f1e63273ac2c0ef3a7c337991966134633423cf6
sergpitk <sergpitk@andi.lv>
2020-02-02 00:30:12 +0200

    sword 0.043 removed setFile field from DocumentController

--- src/Controller/DocumentsController.php
+++ src/Controller/DocumentsController.php
@@ -118,15 +118,21 @@ class DocumentsController extends AbstractController
     /**
      * @Route("/{document}", name="document-get", methods={"GET", "HEAD"} )
      * @param $document
-     * @return JsonResponse
+//     * @return JsonResponse
      */
     public function documentGetHead($document)
     {
-        return $this->json([
+        // <embed src="../pdf/sample-3pp.pdf#page=2" type="application/pdf" width="100%" height="100%">
+
+        return $this->render('documents/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+
+        /*return $this->json([
             'controller_name' => 'DocumentsController',
             'methods_name' => 'documentGetHead',
             'document' => $document
-        ]);
+        ]);*/
     }
 
 
